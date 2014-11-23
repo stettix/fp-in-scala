7bbfc3546354f5ca1e6835571f67bcb3ccd19920
Jan Stette <jan.stette@gmail.com>
2014-11-23 15:02:34 +0000

    More List exercises: map, flatMap and zipWith

--- src/main/scala/net/janvsmachine/fpinscala/Chapter3.scala
+++ src/main/scala/net/janvsmachine/fpinscala/Chapter3.scala
@@ -104,4 +104,49 @@ object Chapter3 extends App {
   val l2 = List(7, 8, 9)
   assert(append(l, l2) == List(1, 2, 3, 4, 5, 6, 7, 8, 9))
 
+  // Exercise 3.16
+  def add1(as: List[Int]): List[Int] = as match {
+    case Nil         ⇒ Nil
+    case Cons(x, xs) ⇒ Cons(x + 1, add1(xs))
+  }
+  assert(add1(l) == List(2, 3, 4, 5, 6, 7))
+
+  def doublesToString(ds: List[Double]): List[String] = ds match {
+    case Nil         ⇒ Nil
+    case Cons(x, xs) ⇒ Cons(x.toString, doublesToString(xs))
+  }
+
+  // Exercise 3.18
+  def map[A, B](as: List[A])(f: A ⇒ B): List[B] = as match {
+    case Nil         ⇒ Nil
+    case Cons(x, xs) ⇒ Cons(f(x), map(xs)(f))
+  }
+  assert(map(l)(_ + 1) == List(2, 3, 4, 5, 6, 7))
+
+  // Exercise 3.19
+  def filter[A](as: List[A])(f: A ⇒ Boolean): List[A] = as match {
+    case Nil                 ⇒ Nil
+    case Cons(x, xs) if f(x) ⇒ Cons(x, filter(xs)(f))
+    case Cons(x, xs)         ⇒ filter(xs)(f)
+  }
+  assert(filter(l)(_ % 2 == 0) == List(2, 4, 6))
+
+  // Exercise 3.20
+  def flatMap[A, B](as: List[A])(f: A ⇒ List[B]): List[B] = as match {
+    case Nil         ⇒ Nil
+    case Cons(x, xs) ⇒ append(f(x), flatMap(xs)(f))
+  }
+  assert(flatMap(l)(x ⇒ List(x, x)) == List(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6))
+
+  // Exercsise 3.21
+  def filterFl[A](as: List[A])(p: A ⇒ Boolean): List[A] =
+    flatMap(as)((a: A) ⇒ if (p(a)) List(a) else Nil)
+  assert(filterFl(l)(_ % 2 == 0) == List(2, 4, 6))
+
+  // Exercise 3.23
+  def zipWith[A](xs: List[A], ys: List[A])(f: (A, A) ⇒ A): List[A] = (xs, ys) match {
+    case (Cons(a, as), Cons(b, bs)) ⇒ Cons(f(a, b), zipWith(as, bs)(f))
+    case _                          ⇒ Nil
+  }
+  assert(zipWith(l, l2)(_ + _) == List(8, 10, 12))
 }

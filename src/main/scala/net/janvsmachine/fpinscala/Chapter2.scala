257fa33a6f6dc901f5b9bd87d7e06220ebdf2d88
Jan Stette <jan.stette@gmail.com>
2014-11-23 08:55:35 +0000

    Chapter 2.

--- src/main/scala/net/janvsmachine/fpinscala/Chapter2.scala
+++ src/main/scala/net/janvsmachine/fpinscala/Chapter2.scala
@@ -8,4 +8,18 @@ object Chapter2 {
     pairs.forall(p ⇒ p._1 == p._2 || ordered(p._1, p._2))
   }
 
+  def partial1[A, B, C](a: A, f: (A, B) ⇒ C): B ⇒ C =
+    (b: B) ⇒ f(a, b)
+
+  // Exercise 2.3.
+  def curry[A, B, C](f: (A, B) ⇒ C): A ⇒ (B ⇒ C) =
+    (a: A) ⇒ (b: B) ⇒ f(a, b)
+
+  // Exercise 2.4.
+  def uncurry[A, B, C](f: A ⇒ B ⇒ C): (A, B) ⇒ C =
+    (a: A, b: B) ⇒ f(a)(b)
+
+  // Exercise 2.5.
+  def compose[A, B, C](f: B => C, g: A => B): A => C = 
+    (a: A) => f(g(a))
 }

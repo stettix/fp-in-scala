a78cd39963b060cbdc1ec23463295d5a068acc56
Jan Stette <jan.stette@gmail.com>
2014-12-05 21:22:18 +0000

    First bit of Stream implementation.

---
+++ src/main/scala/net/janvsmachine/fpinscala/Stream.scala
@@ -0,0 +1,43 @@
+package net.janvsmachine.fpinscala
+
+import Stream._
+
+sealed trait Stream[+A] {
+  def toList: List[A]
+  def headOption: Option[A]
+
+  def exists(p: A ⇒ Boolean): Boolean = this match {
+    case SCons(h, t) ⇒ p(h()) || t().exists(p)
+    case _           ⇒ false
+  }
+
+  def foldRight[B](z: ⇒ B)(f: (A, ⇒ B) ⇒ B): B = this match {
+    case SCons(h, t) ⇒ f(h(), t().foldRight(z)(f))
+    case _           ⇒ z
+  }
+
+  def exists2(p: A ⇒ Boolean): Boolean = foldRight(false)((a, res) ⇒ p(a) || res)
+
+  def forAll(p: A ⇒ Boolean): Boolean = foldRight(true)((a, res) ⇒ p(a) && res)
+
+}
+
+case object Empty extends Stream[Nothing] {
+  def toList: List[Nothing] = Nil
+  def headOption: Option[Nothing] = None
+}
+
+case class SCons[+A](h: () ⇒ A, t: () ⇒ Stream[A]) extends Stream[A] {
+  def toList: List[A] = Cons(h(), t().toList)
+  def headOption: Option[A] = Some(h())
+}
+
+object Stream {
+  def cons[A](hd: ⇒ A, tl: ⇒ Stream[A]) = {
+    lazy val head = hd
+    lazy val tail = tl
+    SCons(() ⇒ head, () ⇒ tail)
+  }
+  def empty[A](): Stream[A] = Empty
+  def apply[A](as: A*): Stream[A] = if (as.isEmpty) Empty else cons(as.head, apply(as.tail: _*))
+}
---
+++ src/test/scala/net/janvsmachine/fpinscala/StreamTests.scala
@@ -0,0 +1,32 @@
+package net.janvsmachine.fpinscala
+
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+import Stream._
+
+@RunWith(classOf[JUnitRunner])
+class StreamTests extends FlatSpec {
+
+  "An empty stream" should "comply with spec" in {
+    assert(empty.toList == Nil)
+    assert(empty.headOption == None)
+  }
+
+  "A stream with values" should "comply with spec" in {
+    assert(Stream(1, 2, 3).toList == List(1, 2, 3))
+    assert(Stream(1, 2, 3).headOption == Some(1))
+  }
+
+  "exists" should "do the right thing yea" in {
+    assert(Stream(1, 2, 3).exists(_ > 2))
+    assert(!empty[Int].exists(_ > 0))
+  }
+
+  "forAll" should "you know" in {
+    assert(empty[Int].forAll(_ ⇒ false))
+    assert(Stream(1, 2, 3).forAll(_ > 0))
+    assert(!Stream(1, 2, 3).forAll(_ < 3))
+  }
+
+}

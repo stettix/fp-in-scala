46d1840af184a8d0f672c5d48a9c83c38bfe03e2
Jan Stette <jan.stette@gmail.com>
2015-01-04 17:29:05 +0000

    Added Rand, with map, map2 etc.

--- src/main/scala/net/janvsmachine/fpinscala/Random.scala
+++ src/main/scala/net/janvsmachine/fpinscala/Random.scala
@@ -20,6 +20,25 @@ case class SimpleRNG(seed: Long) extends RNG {
 
 object RNG {
 
+  type Rand[+A] = RNG ⇒ (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng ⇒ (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A ⇒ B): Rand[B] =
+    rng ⇒ {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) ⇒ C): Rand[C] =
+    rng ⇒ {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (n, nextRNG) = rng.nextInt
     val nextVal = if (n == Integer.MIN_VALUE) Integer.MAX_VALUE else abs(n)
@@ -58,5 +77,14 @@ object RNG {
       (Cons(nextVal, acc._1), nextRng)
     })
   }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i ⇒ i - i % 2)
+
+  def doublev2: Rand[Double] = map(nonNegativeInt)(_.toDouble / Integer.MAX_VALUE)
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2(ra, rb)((_, _))
+
+  def randIntDouble: Rand[(Int, Double)] = both(int, double)
+  def randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
 }

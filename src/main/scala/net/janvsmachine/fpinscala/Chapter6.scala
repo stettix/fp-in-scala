9250d44a75ea51b21244fdfecfa7fdd0388af648
Jan Stette <jan.stette@gmail.com>
2015-01-14 00:38:50 +0000

    Implemented stateful vending machine example.

---
+++ src/main/scala/net/janvsmachine/fpinscala/Chapter6.scala
@@ -0,0 +1,31 @@
+package net.janvsmachine.fpinscala
+
+import States._
+import List._
+
+// Exercise 6.11.
+
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object Machine {
+
+  type MachineState = State[Machine, (Int, Int)]
+
+  def simulateMachine(inputs: List[Input]): MachineState =
+    machine ⇒ {
+      val updated = foldLeft(inputs, machine)((m, input) ⇒ handleInput(m, input))
+      ((updated.coins, updated.candies), updated)
+    }
+
+  def handleInput(machine: Machine, input: Input): Machine =
+    input match {
+      case Coin if machine.locked && machine.candies > 0 ⇒ machine.copy(locked = false, coins = machine.coins + 1)
+      case Coin ⇒ machine
+      case Turn if !machine.locked ⇒ machine.copy(locked = true, candies = machine.candies - 1)
+      case Turn ⇒ machine
+    }
+}
---
+++ src/test/scala/net/janvsmachine/fpinscala/Chapter6Tests.scala
@@ -0,0 +1,42 @@
+package net.janvsmachine.fpinscala
+
+import org.scalatest.FlatSpec
+
+import Machine._
+import States._
+
+class MachineTests extends FlatSpec {
+
+  val emptyMachine = Machine(true, 0, 0)
+  val machineWithCandy = Machine(true, 10, 0)
+
+  "An empty candy dispenser" should "ignore all inputs" in {
+    val allInputs = Seq(Nil, List(Coin), List(Turn))
+    for (inputs ← allInputs) {
+      val ((coins, candies), _) = simulateMachine(inputs)(emptyMachine)
+      assert(coins == 0 && candies == 0)
+    }
+  }
+
+  "A machine with candy" should "give candy when coin is inserted and knob is turned" in {
+    val ((coins, candies), _) = simulateMachine(List(Coin, Turn))(machineWithCandy)
+    assert(coins == machineWithCandy.coins + 1 &&
+      candies == machineWithCandy.candies - 1)
+  }
+
+  it should "not respond to turning knob when no coin has been given" in {
+    val ((coins, candies), _) = simulateMachine(List(Turn))(machineWithCandy)
+    assert(coins == machineWithCandy.coins && candies == machineWithCandy.candies)
+  }
+
+  it should "not give candy until knob is turned" in {
+    val ((coins, candies), _) = simulateMachine(List(Coin))(machineWithCandy)
+    assert(coins == machineWithCandy.coins + 1 && candies == machineWithCandy.candies)
+  }
+
+  it should "dispense candy after several input sequences" in {
+      val ((coins, candies), _) = simulateMachine(List(Coin, Turn, Coin, Turn, Coin, Turn))(machineWithCandy)
+      assert(coins == machineWithCandy.coins + 3 && candies == machineWithCandy.candies - 3)
+  }
+
+}

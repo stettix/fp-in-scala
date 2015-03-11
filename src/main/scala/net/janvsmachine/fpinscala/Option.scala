8e320e7fa7eb211d57a7400a0fc116e4479af88b
Jan Stette <jan.stette@gmail.com>
2015-03-11 17:40:48 +0000

    Updated Option code after review.

--- src/main/scala/net/janvsmachine/fpinscala/Option.scala
+++ src/main/scala/net/janvsmachine/fpinscala/Option.scala
@@ -1,6 +1,16 @@
 package net.janvsmachine.fpinscala
 
-trait Option[+A] {
+//
+// For this one, I've written the functions in the style where each operation
+// is defined in a base trait and implemented in derived objects/classes.
+// It's only a stylistic difference, though looing at it, I do find it neat avoiding
+// the pattern match on the None vs Some cases in each method - and calling the method
+// on values instead of passing values in as the first argument to each function seems neater too.
+//
+// Functions on multiple Options are still defined as free-standing functions, of course.
+//
+
+sealed trait Option[+A] {
   def map[B](f: A ⇒ B): Option[B]
   def flatMap[B](f: A ⇒ Option[B]): Option[B]
   def getOrElse[B >: A](default: B): B
@@ -14,7 +24,7 @@ case class Some[+A](value: A) extends Option[A] {
   def map[B](f: A ⇒ B): Option[B] = Some(f(value))
   def flatMap[B](f: A ⇒ Option[B]): Option[B] = f(value)
   def getOrElse[B >: A](default: B): B = value
-  def orElse[B >: A](default: B): Option[B] = Some(value)
+  def orElse[B >: A](default: B): Option[B] = this
   def filter(f: A ⇒ Boolean): Option[A] = if (f(value)) Some(value) else None
 }
 
@@ -30,15 +40,14 @@ object Option {
 
   import List._
 
+  // From book.
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
   // Exercise 4.2
-  def variance(xs: Seq[Double]): Option[Double] = xs match {
-    case Seq() ⇒ None
-    case values ⇒ {
-      val m = xs.sum / xs.length
-      val divs = xs.foldLeft(0.0d) { case (acc, x) ⇒ acc + Math.pow(x - m, 2) }
-      Some(divs / xs.length)
-    }
-  }
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => Math.pow(x - m, 2))))
 
   // Exercise 4.3
   def map2a[A, B, C](a: Option[A], b: Option[B])(f: (A, B) ⇒ C): Option[C] = (a, b) match {

94ba3f36fa068c6be13b3a03e8b192ec009cb952
Jan Stette <jan.stette@gmail.com>
2021-05-04 16:38:50 +0100

    Add flatMap, tidy up

--- src/main/scala/net/janvsmachine/fpinscala/Parsers.scala
+++ src/main/scala/net/janvsmachine/fpinscala/Parsers.scala
@@ -9,37 +9,32 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
+  // Fundamental combinators
+
   implicit def string(s: String): Parser[String]
 
   def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
-  // Returns the number of repeated values of the given parser seen
-  def zeroOrMore[A](p: Parser[A]): Parser[Int]
-
-  // Returns the number of repeated values of the given parser seen
-  def oneOrMore[A](p: Parser[A]): Parser[Int]
-
-  def zeroOrMoreThenOneOrMore[A](p1: Parser[A], p2: Parser[A]): (Int, Int)
-
-  // Fundamental combinators
-
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     map2(p, listOfN(n - 1, p))(Cons(_, _))
 
-  // Return the part of a string that the parser matches
   def slice[A](p: Parser[A]): Parser[String]
 
-  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)]
-
   // Derived combinators
+
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
+    p.flatMap(f andThen succeed)
+
+  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    p1.flatMap(a => p2.map(a -> _))
 
   def succeed[A](a: A): Parser[A] =
     string("").map(_ => a)
 
   def map2[A, B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
-    product(p1, p2).map(f.tupled)
+    for { a <- p1; b <- p2 } yield f(a, b)
 
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(Cons(_, _)) or succeed(List())
@@ -47,7 +42,6 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))((a, as) => Cons(a, as))
 
-  // Law: for any char 'c', run(char(c))(c.toString) should return Right(c)
   def char(c: Char): Parser[Char] =
     string(c.toString).map(_.charAt(0))
 
@@ -60,9 +54,12 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def **[B >: A](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
-    def many(p: Parser[A]): Parser[List[A]] = self.many(p)
-
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def map2[B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p1, p2)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+    def many(p: Parser[A]): Parser[List[A]] = self.many(p)
+    def many1(p: Parser[A]): Parser[List[A]] = self.many1(p)
+    def slice(p: Parser[A]): Parser[String] = self.slice(p)
 
   }
 
